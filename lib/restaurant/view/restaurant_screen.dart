2dd8802585861fac308a9af264ac961b2131cd28
eunhyeong99 <whdmsgud8@naver.com>
2024-12-02 00:57:40 +0900

    fix: 데이터 모델링

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -38,19 +39,13 @@ class RestaurantScreen extends StatelessWidget {
               return ListView.separated(
                 itemCount: snapshot.data!.length,
                 itemBuilder: (_, index) {
-                  print(index);
                   final item = snapshot.data![index];
-                  return RestaurantCard(
-                   image: Image.network(
-                     'http://$ip${item['thumbUrl']}',
-                     fit: BoxFit.cover,
-                   ),
-                    name: item['name'],
-                    tags: List<String>.from(item['tags']),
-                    ratingsCount: item['ratingsCount'],
-                    deliveryTime: item['deliveryTime'],
-                    deliveryFee: item['deliveryFee'],
-                    ratings: item['ratings'],
+                  final pItem = RestaurantModel.fromJson(
+                    json: item,
+                  );
+
+                  return RestaurantCard.fromModel(
+                    model: pItem,
                   );
                 },
                 separatorBuilder: (_, index) {
